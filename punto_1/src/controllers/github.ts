9c8e49ccf151867b769c4913c7d42d30d1b4a27a
guillermo penaranda <90706729+guille1999utp@users.noreply.github.com>
2024-01-28 22:36:21 -0500

    arreglando enpoint a solicitud de github

--- punto_1/src/controllers/github.ts
+++ punto_1/src/controllers/github.ts
@@ -5,35 +5,19 @@ import { GitHubRepo } from "../interfaces/github";
 export const getRepositorios = async (req: Request, res: Response) => {
   try {
     const username = 'google';
-    let page = 1;
-    let repos: GitHubRepo[] = [];
+    const perPage = 10;
 
-    while (true) {
-      const response: AxiosResponse<any[]> = await axios.get(
-        `https://api.github.com/users/${username}/repos?sort=stargazers_count&page=${page}&per_page=100`
-      );
-        console.log(response.data.length)
-      if (response.data.length === 0) {
-        break;
-      }
+    const response: AxiosResponse<any> = await axios.get(
+      `https://api.github.com/search/repositories?q=user:${username}&per_page=${perPage}`
+    );
 
-      repos = [
-        ...repos,
-        ...response.data.map((repo) => ({
-          name: repo.name,
-          description: repo.description,
-          stargazers_count: repo.stargazers_count
-        })),
-      ];
+    const repos: GitHubRepo[] = response.data.items.map((repo: any) => ({
+      name: repo.name,
+      description: repo.description,
+      stargazers_count: repo.stargazers_count
+    }));
 
-      page++;
-    }
-    console.log(repos.length)
-    repos.sort((a, b) => b.stargazers_count - a.stargazers_count);
-
-    const topRepos = repos.slice(0, 10);
-
-    return res.status(200).json({ ok: true, repositorios: topRepos });
+    return res.status(200).json({ ok: true, repositorios: repos });
   } catch (error: any) {
     console.error(error.message);
     res.status(500).json({ ok: false, error: error.message });
